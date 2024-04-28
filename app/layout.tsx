41e4a0ea438c187d895890313eff6d27ef85f74c
Diyor <diyornuriddinov06@gmail.com>
2024-04-28 20:57:14 +0500

    Stream Create Meeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-
+import { Toaster } from '@/components/ui/toaster'
+import { ReactNode } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -13,24 +14,29 @@ export const metadata: Metadata = {
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode
+	children: ReactNode
 }>) {
 	return (
 		<html lang='en'>
-			<ClerkProvider appearance={{
-				layout: {
-					logoImageUrl: '/icons/yoom-logo.svg',
-					socialButtonsVariant: 'iconButton'
-				},
-				variables: {
-					colorText: '#fff',
-					colorPrimary: '#0e78f9',
-					colorBackground: '#1c1f2e',
-					colorInputBackground: '#252a41',
-					colorInputText: '#fff'
-				}
-			}}>
-				<body className={`${inter.className} bg-dark-2`}>{children}</body>
+			<ClerkProvider
+				appearance={{
+					layout: {
+						logoImageUrl: '/icons/yoom-logo.svg',
+						socialButtonsVariant: 'iconButton',
+					},
+					variables: {
+						colorText: '#fff',
+						colorPrimary: '#0e78f9',
+						colorBackground: '#1c1f2e',
+						colorInputBackground: '#252a41',
+						colorInputText: '#fff',
+					},
+				}}
+			>
+				<body className={`${inter.className} bg-dark-2`}>
+					{children}
+					<Toaster />
+				</body>
 			</ClerkProvider>
 		</html>
 	)
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,13 +3,62 @@ import { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { toast } from './ui/use-toast'
+import Loader from './Loader'
+
+const initialValues = {
+	dateTime: new Date(),
+	description: '',
+	link: '',
+}
 
 const MeetingTypeList = () => {
 	const router = useRouter()
 	const [meetingState, setMeetingState] = useState<
 		'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
 	>()
-	const createMeeting = () => {}
+	const { user } = useUser()
+	const client = useStreamVideoClient()
+	const [values, setValues] = useState(initialValues)
+	const [callDetail, setCallDetail] = useState<Call>()
+	const createMeeting = async () => {
+		if (!client || !user) return
+		try {
+			if (!values.dateTime) {
+				toast({ title: 'Please select a date and time' })
+				return
+			}
+
+			const id = crypto.randomUUID()
+			const call = client.call('default', id)
+			if (!call) throw new Error('Failed to create call')
+			const starsAt =
+				values.dateTime.toISOString() || new Date(Date.now()).toISOString()
+			const description = values.description || 'Instant meeting'
+			await call.getOrCreate({
+				data: {
+					starts_at: starsAt,
+					custom: {
+						description,
+					},
+				},
+			})
+			setCallDetail(call)
+			if (!values.description) {
+				router.push(`/meeting/${call.id}`)
+			}
+			toast({
+				title: 'Meeting Created',
+			})
+		} catch (error) {
+			console.log(error)
+			toast({ title: 'Failed to create Meeting' })
+		}
+	}
+	if (!client || !user) return <Loader />
+
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
 			<HomeCard
