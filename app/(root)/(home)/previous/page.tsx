69aa1e8f18ff7459dd841aec32de67546ebf2e6b
Diyor <diyornuriddinov06@gmail.com>
2024-04-28 22:42:15 +0500

    Previous Meetings Page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,8 +1,13 @@
-const Previous = () => {
-	return (
-		<section className='flex size-full flex-col gap-10 text-white'>
-			<h1 className='text-3xl font-bold'>Previous</h1>
-		</section>
-	)
-}
-export default Previous
+import CallList from "@/components/CallList";
+
+const PreviousPage = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous Calls</h1>
+
+      <CallList type="ended" />
+    </section>
+  );
+};
+
+export default PreviousPage;
